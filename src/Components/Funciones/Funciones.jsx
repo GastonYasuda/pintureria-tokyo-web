dc17267dcd2f7338505c663c201c42e796d9b393
GastonYasuda <gastonyasuda@gmail.com>
2023-12-10 20:49:24 -0300

    misDatos style

--- src/Components/Funciones/Funciones.jsx
+++ src/Components/Funciones/Funciones.jsx
@@ -4,21 +4,21 @@ const Funciones = () => {
     return (
         <div className='funciones'>
             <section className='funciones__box d-f-center'>
-                <img src="./assets/estrella.png" alt="" />
+                <img src="./assets/estrella.svg" alt="" />
                 <div className='funciones__box--txt'>
                     <h4>ASESORAMIENTO</h4>
                     <p>Atención personalizada con cada uno de nuestros clientes.</p>
                 </div>
             </section>
             <section className='funciones__box d-f-center'>
-                <img src="./assets/check.png" alt="" />
+                <img src="./assets/check.svg" alt="" />
                 <div className='funciones__box--txt'>
                     <h4>GARANTÍA Y RESPALDO</h4>
                     <p>Somos distribuidores certificados de Sherwin Williams.</p>
                 </div>
             </section>
             <section className='funciones__box d-f-center'>
-                <img src="./assets/envio.png" alt="" />
+                <img src="./assets/envio.svg" alt="" />
                 <div className='funciones__box--txt'>
                     <h4>ENVÍO SIN CARGO</h4>
                     <p>A Capital y Gran Buenos Aires.</p>
--- src/Components/MisDatos/MisDatos.jsx
+++ src/Components/MisDatos/MisDatos.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 const MisDatos = () => {
     return (
-        <div>MisDatos</div>
+        <section className='misDatos d-f-col-center'>
+            <div className="misDatos__datos">
+                <div className='misDatos__datos-animate'>
+                    <img src="./assets/corazon.svg" alt="" />
+                    <h2>ANIMATE</h2>
+                    <p>Subí tu foto y proba los colores y estilos que más te gustan para renovar las habitaciones de tu casa.</p>
+                    <button>Simulador de Colores</button>
+                </div>
+                <div className='misDatos__datos-contacto'>
+                    <img src="./assets/telefono.svg" alt="" />
+                    <h2>CONTACTATE</h2>
+                    <h4>4622-2824 / 4462-6965</h4>
+                    <p>Llevamos 40 años brindando la mejor atención y asesoramiento para que la compra de tus productos se adapte específicamente a lo que necesitas.</p>
+                    <button>Contactanos por Whatsapp</button>
+                </div>
+            </div>
+            <div className="misDatos__mapa">
+                <div className="misDatos__mapa-txt">
+                    <img src="./assets/ubicacion.svg" alt="" />
+                    <h2>ACERCATE</h2>
+                    <p>En nuestros locales de Tapiales, Villa Celina y Ciudad Madero vas a encontrar todo lo que necesitas para tus proyectos.</p>
+                </div>
+                <div className="misDatos__mapa-mapa">
+                    MAPA
+                </div>
+            </div>
+        </section>
     )
 }
 
